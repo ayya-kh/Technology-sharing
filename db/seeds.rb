91d41c7fae034c41c0f27b9e50e591b2ddd00224
EC2 Default User <ec2-user@ip-172-31-31-146.ap-northeast-1.compute.internal>
2023-01-31 11:34:33 +0000

    テストデータコメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-tags = Tag.create!([
+Tag.create!([
   { name: 'C言語' },
   { name: 'Java' },
   { name: 'HTML' },
@@ -15,21 +15,31 @@ tags = Tag.create!([
   { name: 'Ruby on Rails' },
 ])
 
-# Admin.create!(email:"admin@test",password:"testtest")
+Admin.create!(email:"admin@test",password:"testtest")
 
 #テストデータ
-users = User.create!(
-  [
-    {email: 'satou@test', name: 'Satou', password: 'satoru_pass'},
-    {email: 'suzuki@test', name: 'Suzuki', password: 'suzuki_pass'},
-    {email: 'takahashi@test', name: 'Takahashi', password: 'takahashi_pass'}
-  ]
-)
 
-Contribution.create!(
-  [
-    {title: '10秒カウントダウン動画', text: '10秒のカウントダウン動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/【無料動画素材】10秒カウントダウン.mp4"), filename: '【無料動画素材】10秒カウントダウン.mp4'), tag_ids: [tags[0].id], user_id: users[0].id },
-    {title: '20秒サンプル動画', text: '20秒のサンプル動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/20秒もない曲.mp4"), filename: '20秒もない曲.mp4'), tag_ids: [tags[1].id], user_id: users[1].id },
-    {title: '30秒BGM動画', text: '30秒のBGM動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/30秒くらいの曲を作ってみた。.mp4"), filename: '30秒くらいの曲を作ってみた。.mp4'), tag_ids: [tags[2].id], user_id: users[2].id }
-  ]
-)
+# tags = Tag.create!([
+#   { name: 'C言語' },
+#   { name: 'Java' },
+#   { name: 'HTML' },
+#   { name: 'CSS' },
+#   { name: 'Ruby' },
+#   { name: 'Ruby on Rails' },
+# ])
+
+# users = User.create!(
+#   [
+#     {email: 'satou@test', name: 'Satou', password: 'satoru_pass'},
+#     {email: 'suzuki@test', name: 'Suzuki', password: 'suzuki_pass'},
+#     {email: 'takahashi@test', name: 'Takahashi', password: 'takahashi_pass'}
+#   ]
+# )
+
+# Contribution.create!(
+#   [
+#     {title: '10秒カウントダウン動画', text: '10秒のカウントダウン動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/【無料動画素材】10秒カウントダウン.mp4"), filename: '【無料動画素材】10秒カウントダウン.mp4'), tag_ids: [tags[0].id], user_id: users[0].id },
+#     {title: '20秒サンプル動画', text: '20秒のサンプル動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/20秒もない曲.mp4"), filename: '20秒もない曲.mp4'), tag_ids: [tags[1].id], user_id: users[1].id },
+#     {title: '30秒BGM動画', text: '30秒のBGM動画です', video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/30秒くらいの曲を作ってみた。.mp4"), filename: '30秒くらいの曲を作ってみた。.mp4'), tag_ids: [tags[2].id], user_id: users[2].id }
+#   ]
+# )
