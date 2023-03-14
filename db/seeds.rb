1d10bc31e5011f4fd5746723a6cb118e1d20706a
ayya-kh <ay37346497@gmail.com>
2023-03-14 09:12:46 +0000

    テスト

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#テストデータ時にはコメントアウト
 Tag.create!([
   { name: 'C言語' },
   { name: 'Java' },
